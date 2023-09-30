d347448d12d7f6846f9259b4877c9394dec7ef0d
Dani Mazza <danielguillouxmazza@gmail.com>
2023-09-29 22:01:51 -0300

    "teste commit"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I'm NB")
+        System.out.println("I'm NB");
+        System.out.println("I love y S2");
     }
 }
